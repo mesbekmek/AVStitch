25717056670dbc60238900bc320212b9af684ece
Mesfin Bekele Mekonnen <mesbekmek@gmail.com>
2016-04-26 12:50:30 -0400

    Initial Commit

---
+++ Example/Pods/Target Support Files/AVStitch/AVStitch-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AVStitch : NSObject
+@end
+@implementation PodsDummy_AVStitch
+@end
---
+++ Example/Pods/Target Support Files/AVStitch/AVStitch-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double AVStitchVersionNumber;
+FOUNDATION_EXPORT const unsigned char AVStitchVersionString[];
+
